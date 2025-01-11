80ef2968deec3ad24e6f7823f0eeec9c0573db7d
itspalmera <88901304+itspalmera@users.noreply.github.com>
2025-01-10 23:07:48 -0300

    first commit

---
+++ src/app/Catedra3/pages/registrer/registrer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrer',
+  imports: [],
+  templateUrl: './registrer.component.html',
+  styleUrl: './registrer.component.css'
+})
+export class RegistrerComponent {
+
+}
